d589b41bdbbea3cf8f5ef7461048149f130b68fc
FELIX DUX <50945610+FelixDux@users.noreply.github.com>
2021-08-05 22:40:07 +0100

    More test refactorign

--- imposc/test/test_main.py
+++ imposc/test/test_main.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+import pytest
 
 from main import app
 
@@ -8,18 +9,13 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Watch this space ..."}
-
-def test_read_inexistent_item():
-    response = client.get("/api/parameter-info/houses")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Not Found"}
-
-def test_read_parameter_symbols():
-    response = client.get("/api/parameter-info/symbols")
-    assert response.status_code == 200
-    assert response.json() == {"Properties":[{"Parameter":"frequency","Property":"ω"},{"Parameter":"offset","Property":"σ"},{"Parameter":"phi","Property":"φ"}]}
-
-def test_read_parameter_groups():
-    response = client.get("/api/parameter-info/groups")
-    assert response.status_code == 200
-    assert response.json() == {"Properties":[{"Parameter":"frequency","Property":"System parameters"},{"Parameter":"offset","Property":"System parameters"},{"Parameter":"r","Property":"System parameters"},{"Parameter":"phi","Property":"Initial impact"},{"Parameter":"v","Property":"Initial impact"},{"Parameter":"maxPeriods","Property":"Control parameters"},{"Parameter":"numIterations","Property":"Control parameters"},{"Parameter":"numPoints","Property":"Control parameters"}]}
+    
+@pytest.mark.parametrize(('path', 'status', 'response_json'), [
+    ("symbols", 200, {"Properties":[{"Parameter":"frequency","Property":"ω"},{"Parameter":"offset","Property":"σ"},{"Parameter":"phi","Property":"φ"}]}),
+    ("groups", 200, {"Properties":[{"Parameter":"frequency","Property":"System parameters"},{"Parameter":"offset","Property":"System parameters"},{"Parameter":"r","Property":"System parameters"},{"Parameter":"phi","Property":"Initial impact"},{"Parameter":"v","Property":"Initial impact"},{"Parameter":"maxPeriods","Property":"Control parameters"},{"Parameter":"numIterations","Property":"Control parameters"},{"Parameter":"numPoints","Property":"Control parameters"}]}),
+    ("garbage", 404, {"detail": "Not Found"})
+])
+def test_read_parameter_info(path, status, response_json):
+    response = client.get(f"/api/parameter-info/{path}")
+    assert response.status_code == status
+    assert response.json() == response_json
