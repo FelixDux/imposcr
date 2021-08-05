456a5123870bed435dca524fd1aa300025b2dd5c
FELIX DUX <50945610+FelixDux@users.noreply.github.com>
2021-08-05 22:50:46 +0100

    Refactored to use path parameters

--- imposc/main.py
+++ imposc/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from adapters import parameter_info
 
 app = FastAPI()
@@ -8,10 +8,11 @@ app = FastAPI()
 async def read_main():
     return {"msg": "Watch this space ..."}
 
-@app.get("/api/parameter-info/symbols")
-async def read_parameter_symbols():
-    return parameter_info("symbols")
+@app.get("/api/parameter-info/{category}")
+async def read_parameter_info(category):
+    result = parameter_info(category)
 
-@app.get("/api/parameter-info/groups")
-async def read_parameter_symbols():
-    return parameter_info("groups")
+    if result is None:
+        raise HTTPException(status_code=404, detail="Category not found")
+    else:
+        return result
--- imposc/test/test_main.py
+++ imposc/test/test_main.py
@@ -9,13 +9,13 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Watch this space ..."}
-    
-@pytest.mark.parametrize(('path', 'status', 'response_json'), [
+
+@pytest.mark.parametrize(('category', 'status', 'response_json'), [
     ("symbols", 200, {"Properties":[{"Parameter":"frequency","Property":"ω"},{"Parameter":"offset","Property":"σ"},{"Parameter":"phi","Property":"φ"}]}),
     ("groups", 200, {"Properties":[{"Parameter":"frequency","Property":"System parameters"},{"Parameter":"offset","Property":"System parameters"},{"Parameter":"r","Property":"System parameters"},{"Parameter":"phi","Property":"Initial impact"},{"Parameter":"v","Property":"Initial impact"},{"Parameter":"maxPeriods","Property":"Control parameters"},{"Parameter":"numIterations","Property":"Control parameters"},{"Parameter":"numPoints","Property":"Control parameters"}]}),
-    ("garbage", 404, {"detail": "Not Found"})
+    ("garbage", 404, {"detail": "Category not found"})
 ])
-def test_read_parameter_info(path, status, response_json):
-    response = client.get(f"/api/parameter-info/{path}")
+def test_read_parameter_info(category, status, response_json):
+    response = client.get(f"/api/parameter-info/{category}")
     assert response.status_code == status
     assert response.json() == response_json
