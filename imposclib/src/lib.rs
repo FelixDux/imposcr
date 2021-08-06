7137b6cfb3b450e853ddb5fd4e3ea8ff998dd39d
FELIX DUX <50945610+FelixDux@users.noreply.github.com>
2021-08-06 13:27:01 +0100

    atomic adder from Rust parameter properties

--- imposclib/src/lib.rs
+++ imposclib/src/lib.rs
@@ -1,8 +1,10 @@
 extern crate libc;
+use std::convert::From;
 
 use libc::c_char;
 use std::collections::HashMap;
-// use std::ffi::CStr;
+use std::ffi::CStr;
+use std::ffi::CString;
 
 pub struct ParameterProperties {
     properties: HashMap<String, String>,
@@ -10,21 +12,29 @@ pub struct ParameterProperties {
 
 impl ParameterProperties {
     fn new() -> ParameterProperties
- {
+    {
         ParameterProperties
-     {
+        {
             properties: HashMap::new(),
         }
     }
 
-    fn populate(&mut self, records: Vec<(String, String)>) {
-        self.properties.clear();
-
-        records.into_iter().for_each(|(parameter, property)| {self.properties.entry(parameter).or_insert(property);});
+    fn add(&mut self, parameter: &str, property: &str) {
+        self.properties.entry(String::from(parameter)).or_insert(String::from(property));
     }
 
     fn property(&self, parameter: &str) -> String {
         self.properties.get(parameter).cloned().unwrap_or(String::from(""))
+    }
+}
+
+impl From<Vec<(&str, &str)>> for ParameterProperties {
+    fn from(records: Vec<(&str, &str)>) -> ParameterProperties {
+        let mut properties = ParameterProperties::new();
+
+        records.into_iter().for_each(|(parameter, property)| {properties.add(parameter, property);});
+
+        properties
     }
 }
 
@@ -75,8 +85,7 @@ mod tests {
 
     #[test]
     fn can_access_parameter_properties() {
-        let mut properties = ParameterProperties::new();
-        properties.populate(vec![(String::from("frequency"), String::from("ω"))]);
+        let mut properties = ParameterProperties::from(vec![("frequency", "ω")]);
         assert_eq!(properties.property("frequency"), String::from("ω"));
         assert_eq!(properties.property("period"), String::from(""));
     }
