ec964631997eaa9e53b0545c2cec96c809eae793
Felix Dux <felixjdux@gmail.com>
2021-08-28 10:13:24 +0100

    some more phase converter tests

--- imposclib/src/dynamics/forcing_phase.rs
+++ imposclib/src/dynamics/forcing_phase.rs
@@ -28,31 +28,15 @@ fn new_phase_converter(frequency: f64) -> Result<PhaseConverter, PhaseError> {
 
 impl PhaseConverter {
 
-// func NewPhaseConverter(frequency float64) (*PhaseConverter, error) {
-//     if (frequency == 0) {
-//         return nil, parameters.ZeroForcingFrequencyError(frequency)
-//     }
+    fn time_to_phase(self, simtime: f64) -> f64 {
+        let scaled_time = simtime / self.period;
 
-//     if (frequency < 0) {
-//         return nil, parameters.NegativeForcingFrequencyError(frequency)
-//     }
+        scaled_time - scaled_time.floor()
+    }
 
-//     period := 2.0 * math.Pi / frequency
-
-//     return &PhaseConverter{
-//         Period: period,
-//     }, nil
-// }
-
-// func (converter PhaseConverter) TimeToPhase(simtime float64) float64 {
-//     scaled_time := simtime / converter.Period
-
-//     return scaled_time - math.Floor(scaled_time)
-// }
-
-// func (converter PhaseConverter) TimeIntoCycle (phase float64) float64 {
-//     return phase * converter.Period
-// }
+fn time_into_cycle (self, phase: f64) -> f64 {
+    phase * self.period
+}
 
 // func (converter PhaseConverter) ForwardToPhase (starttime float64, phase float64) float64 {
 //     phase_change := phase - converter.TimeToPhase(starttime)
@@ -85,4 +69,90 @@ mod tests {
         assert_eq!(new_phase_converter(frequency).unwrap_err(), PhaseError::NegativeForcingFrequency{frequency});
     }
 
+    #[test]
+    fn time_converts_to_phase_correctly() -> Result<(), PhaseError> {
+        let converter = new_phase_converter(PI)?;
+
+        let time = 3.0;
+        let expected = 0.5;
+
+        assert_eq!(converter.time_to_phase(time), expected);
+
+        Ok(())
+    }
+
+    #[test]    
+    fn convert_time_into_cycle() -> Result<(), PhaseError> {
+        let phase = 0.80;
+        let period = 1.25;
+        let expected = 1.0;
+        let f = 2.0*PI/period;
+    
+        assert_eq!(new_phase_converter(f)?.time_into_cycle(phase), expected);
+    
+        Ok(())
+    }
+    
+    // var ints = [] int {1, 2, 4, 5, 16}
+    // var frequencies = [] float64 {4.89, 2.76}
+    // var start_time = 0.02
+    
+    // const tol = 1e-6
+    // var opt = cmp.Comparer(func(x, y float64) bool {
+    //     return math.Abs(x-y) < tol
+    // })
+    
+    // func TestShiftTimeInPeriods(t *testing.T) {
+    //     for _, f := range frequencies {
+    //         conv, _ := NewPhaseConverter(f)
+    
+    //         for _, i := range ints {
+    //             time_shift := float64(i)*conv.Period
+    
+    //             shifted_time := time_shift + start_time
+    
+    //             new_time := conv.TimeIntoCycle(conv.TimeToPhase(shifted_time))
+    
+    //             if !cmp.Equal(new_time, start_time, opt) {
+    //                 t.Errorf("Converter with frequency %g does not convert consistently in both directions (start time %g, time shift %g, end time %g, %d periods)",
+    //                         f, start_time, time_shift, new_time, i)
+    //             }
+    
+    //             n := conv.DifferenceInPeriods(start_time, shifted_time)
+    //             if i != n {
+    //                 t.Errorf("Converter with frequency %g does not return correct number of periods %g between times %g and %g: expected %d, got %d",
+    //                         f, conv.Period, start_time, shifted_time, i, n)                
+    //             }
+    //         }
+    //     }
+    // }
+    
+    // func TestForwardToPhase(t *testing.T) {
+    //     for _, f := range frequencies {
+    //         conv, _ := NewPhaseConverter(f)
+    
+    //         for _, i := range ints {
+    //             phase := 0.6
+    //             small_time := 0.2
+    //             big_time := 0.8
+    
+    //             time_delta := float64(i) * conv.Period
+    
+    //             new_small := conv.ForwardToPhase(time_delta + small_time, phase)
+    //             new_small_phase := conv.TimeToPhase(new_small)
+    
+    //             if !cmp.Equal(phase, new_small_phase, opt) {
+    //                 t.Errorf("Phase converter with frequency %g runs forward time %g from time %g to phase %g, expected %g", f, time_delta, small_time, new_small_phase, phase)
+    //             }
+    
+    //             new_big := conv.ForwardToPhase(time_delta + big_time, phase)
+    //             new_big_phase := conv.TimeToPhase(new_big)
+    
+    //             if !cmp.Equal(phase, new_big_phase, opt) {
+    //                 t.Errorf("Phase converter with frequency %g runs forward time %g from time %g to phase %g, expected %g", f, time_delta, big_time, new_big_phase, phase)
+    //             }
+    //         }
+    //     }
+    // }
+    
 }
