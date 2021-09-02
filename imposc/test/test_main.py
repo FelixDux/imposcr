0bb54856cf8494f04b68023c5511e63063ede672
Felix Dux <felixjdux@gmail.com>
2021-09-02 12:40:32 +0100

    pytest refactor

--- imposc/test/test_main.py
+++ imposc/test/test_main.py
@@ -5,6 +5,11 @@ from main import app
 
 client = TestClient(app)
 
+def get_response_for_test(path, status):
+    response = client.get(path)
+    assert response.status_code == status
+    return response.json()
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
@@ -14,9 +19,11 @@ def test_read_main():
     ("groups", 200, {"Properties":[{"Parameter":"frequency","Property":"System parameters"},{"Parameter":"offset","Property":"System parameters"},{"Parameter":"r","Property":"System parameters"},{"Parameter":"phi","Property":"Initial impact"},{"Parameter":"v","Property":"Initial impact"},{"Parameter":"maxPeriods","Property":"Control parameters"},{"Parameter":"numIterations","Property":"Control parameters"},{"Parameter":"numPoints","Property":"Control parameters"}]}),
 ])
 def test_read_parameter_info(category, status, response_json):
-    response = client.get(f"/api/parameter-info/{category}")
-    assert response.status_code == status
-    json = response.json()
+    # response = client.get(f"/api/parameter-info/{category}")
+    # assert response.status_code == status
+    # json = response.json()
+
+    json = get_response_for_test(f"/api/parameter-info/{category}", status)
     
     assert "Properties" in json
 
